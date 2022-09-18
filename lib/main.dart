be8122a99a11e48670121673ce0eca19ea4b2b2f
Antônio Marcos <antoniomarcos.s.rosa@gmail.com>
2022-09-18 12:29:03 -0300

    Aula 3

--- lib/main.dart
+++ lib/main.dart
@@ -5,13 +5,31 @@ main() => runApp(PerguntaApp());
 class PerguntaApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
+    final perguntas = [
+      'Qua a sua cor favorita?',
+      'Qual o seu animal favorito?'
+    ];
+
     return MaterialApp(
       home: Scaffold(
-        appBar: AppBar(
-          title: Text('Perguntas')
-        ),
-        body: Text('Olá Flutter'),
-      ),
+          appBar: AppBar(title: const Text('Perguntas')),
+          body: Column(
+            children: <Widget>[
+              Text(perguntas[0]),
+              const ElevatedButton(
+                onPressed: null,
+                child: Text('Resposta 1')
+              ),
+              const ElevatedButton(
+                onPressed: null,
+                child: Text('Resposta 2')
+              ),
+              const ElevatedButton(
+                onPressed: null,
+                child: Text('Resposta 3')
+              )
+            ],
+          )),
     );
   }
 }
